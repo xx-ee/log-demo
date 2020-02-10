c038ab558060a030b0bc9982c8538c64e4c9b5df
xiedong <xiedong@rmitec.cn>
2020-02-10 13:35:46 +0800

    test1.1

---
+++ Data-view/src/main/java/com/log/mapper/Tongji2Mapper.java
@@ -0,0 +1,7 @@
+package com.log.mapper;
+
+import com.log.base.BaseMapper;
+import com.log.model.Tongji2;
+
+public interface Tongji2Mapper extends BaseMapper<Tongji2> {
+}
---
+++ Data-view/src/main/java/com/log/model/Tongji2.java
@@ -0,0 +1,106 @@
+package com.log.model;
+
+import java.util.Date;
+import javax.persistence.*;
+
+@Table(name = "tongji2")
+public class Tongji2 {
+    @Id
+    @Column(name = "reportTime")
+    @GeneratedValue(generator = "JDBC")
+    private Date reporttime;
+
+    private Integer pv;
+
+    private Integer uv;
+
+    private Integer vv;
+
+    private Integer newip;
+
+    private Integer newcust;
+
+    /**
+     * @return reportTime
+     */
+    public Date getReporttime() {
+        return reporttime;
+    }
+
+    /**
+     * @param reporttime
+     */
+    public void setReporttime(Date reporttime) {
+        this.reporttime = reporttime;
+    }
+
+    /**
+     * @return pv
+     */
+    public Integer getPv() {
+        return pv;
+    }
+
+    /**
+     * @param pv
+     */
+    public void setPv(Integer pv) {
+        this.pv = pv;
+    }
+
+    /**
+     * @return uv
+     */
+    public Integer getUv() {
+        return uv;
+    }
+
+    /**
+     * @param uv
+     */
+    public void setUv(Integer uv) {
+        this.uv = uv;
+    }
+
+    /**
+     * @return vv
+     */
+    public Integer getVv() {
+        return vv;
+    }
+
+    /**
+     * @param vv
+     */
+    public void setVv(Integer vv) {
+        this.vv = vv;
+    }
+
+    /**
+     * @return newip
+     */
+    public Integer getNewip() {
+        return newip;
+    }
+
+    /**
+     * @param newip
+     */
+    public void setNewip(Integer newip) {
+        this.newip = newip;
+    }
+
+    /**
+     * @return newcust
+     */
+    public Integer getNewcust() {
+        return newcust;
+    }
+
+    /**
+     * @param newcust
+     */
+    public void setNewcust(Integer newcust) {
+        this.newcust = newcust;
+    }
+}
